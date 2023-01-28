9961863941ad8c27844d51f179d88c323a84b58d
khushalmeh <khushalmehta96@gmail.com>
2023-01-28 20:49:08 +0530

    kjkik

--- javascript/responsive.js
+++ javascript/responsive.js
@@ -9,3 +9,24 @@ burger.addEventListener('click', ()=>{
     navlist.classList.toggle('v-class-resp')
     navbar.classList.toggle('h-nav-resp')
 })
+function contact()
+{
+   let input1 = document.forms['form']['input'];
+   let input2= document.forms['form']['phone'];
+   let k = /[0-9]/;
+   if(input1.value == "")
+   {
+    alert("Please fill the name");
+    return false;
+   }
+  else if(input2.value.match.length < 10 || input2.value.match.length > 10)
+   {
+    alert("Please Enter valid Phone Number");
+    return false;
+   }
+   else{
+    alert("Successfully Submitted!");
+    return true;
+   }
+
+}
